b2e6f18e926014f271624ab9f1c16069393ad128
baydenusa <115723601+baydenusa@users.noreply.github.com>
2022-10-13 17:58:44 +0300

    Add files via upload

---
+++ green-hubabub.py
@@ -0,0 +1,67 @@
+def hiza(health):
+         if (health < 0):
+                  print("Собака Алишера мертва")
+         else :
+                  print("Еще жива")
+
+
+print("ОС Зеленая хуба-буба")
+print("Выберите дейтсвие ОС:")
+print("1.Гусь")
+print("2.2-Кинотеатр")
+print("3.Сервис эл-почты Почта и точка")
+print("4.2-АРКТИКА")
+print("5.2Tube")
+print("6.2Школа")
+choose = input(":")
+if (choose == "1"):
+    a = "goose"
+    print(a)
+    
+if (choose == "2"):
+    print("2 Кинотеатр 0.0.1")
+    print("Название фильма")
+    filmname = input(":")
+    print("Введите адрес кинотеатра KINOSTAR(R)")
+    kinoteat = input(":")
+    print("Время")
+    vremya = input(":")
+    print("Билет на",filmname,"в кинотеатре",kinoteat,"на время",vremya,"забронирован")
+    
+if (choose == "3"):
+    
+    print("ПОЧТА И ТОЧКА Введите адрес")
+    pochta = input(":")
+    print("LOGIN:")
+    login = input(":")
+    if ("@" in pochta and "@" not in login):
+        print("Успешная регистрация в ПОЧТА И ТОЧКА")
+if (choose == "4"):
+         print("*" * 5 , "Добро пожаловать! В 2-Арктика от 2`с, Welcome! To 2-Arctica by 2`S", "*" * 5  )
+         proezd = input(":")
+         if (proezd == "sim"):
+                  healthy = input()
+                  hiza(int(healthy))
+if (choose == "5"):
+         name = input("ИМЯ")
+         print("Возраст")
+         age = input()
+         agei = int(age)
+         if (agei < 18):
+                  print("Доступ запрещен!Родительский контроль! Вы,", name , "не можете смотреть в 2-Tube!")
+         else :
+                  
+                  print("Добро пожаловать в 2-Tube, ", name, "Узнайте подробнее о продуктах 2`C на сайте http://openvk.su/twop")
+
+if (choose == "6"):
+    mrname = input()
+    print("Привет,",mrname)
+                  
+
+
+                  
+         
+    
+        
+
+                
